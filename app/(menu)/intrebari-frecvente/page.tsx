955873ca1a862147f3654e942a275b4ef4084d18
Luca <lucadumi07@gmail.com>
2023-10-13 13:41:46 +0300

    eleventh commit

---
+++ app/(menu)/intrebari-frecvente/page.tsx
@@ -0,0 +1,22 @@
+import Title from "@/app/components/layout/Title";
+
+const FAQPage = () => {
+  return (
+    <div className="w-full h-full">
+      <section className="px-20 mb-6">
+        <div className="flex items-center gap-3 mb-6">
+          <p>Acasa</p>
+          <p className="text-primary">/</p>
+          <p className="text-primary">Intrebari frecvente</p>
+        </div>
+        <div className="mb-14">
+          <Title title="Intrebari frecvente" />
+        </div>
+      </section>
+
+      <section className="pl-20 pr-96 mb-24 flex flex-col gap-8"></section>
+    </div>
+  );
+};
+
+export default FAQPage;
