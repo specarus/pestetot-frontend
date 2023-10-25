5471d71b7baf24a7827dd69ac0ee0b4851d7caad
Luca <lucadumi07@gmail.com>
2023-10-25 16:41:29 +0300

    sixty-ninth commit

--- app/contexts/UserContext.tsx
+++ app/contexts/UserContext.tsx
@@ -305,7 +305,7 @@ const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
         .then((res) => setUser(res.data));
       setIsMounted(true);
     }
-  }, [user._id]);
+  }, []);
 
   return (
     <UserContext.Provider
