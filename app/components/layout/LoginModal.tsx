7612d0e236d92ce6eb82abd71d86789f1d6269c1
Luca <lucadumi07@gmail.com>
2023-10-26 20:35:46 +0300

    seventy-sixth commit

--- app/components/layout/LoginModal.tsx
+++ app/components/layout/LoginModal.tsx
@@ -60,24 +60,25 @@ const LoginModal: React.FC<LoginModalProps> = ({
             timerProgressBar: "bg-red-500",
           },
         });
+      } else {
+        Swal.fire({
+          position: "top",
+          timer: 2000,
+          backdrop: "transparent",
+          title: "V-ati conectat cu succes!",
+          timerProgressBar: true,
+          showConfirmButton: false,
+          customClass: {
+            popup: "w-auto h-auto laptop:px-4 pb-2",
+            title: "text-sm font-normal",
+            timerProgressBar: "bg-green-500",
+          },
+        });
+        router.push("/contul-meu");
+        setShowAccountModal(false);
+        resetLoginFocuses();
+        resetLoginUser();
       }
-      Swal.fire({
-        position: "top",
-        timer: 2000,
-        backdrop: "transparent",
-        title: "V-ati conectat cu succes!",
-        timerProgressBar: true,
-        showConfirmButton: false,
-        customClass: {
-          popup: "w-auto h-auto laptop:px-4 pb-2",
-          title: "text-sm font-normal",
-          timerProgressBar: "bg-green-500",
-        },
-      });
-      router.push("/contul-meu");
-      setShowAccountModal(false);
-      resetLoginFocuses();
-      resetLoginUser();
     } catch (error) {
       console.log(error);
     }
